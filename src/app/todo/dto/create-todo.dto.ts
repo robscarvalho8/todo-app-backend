15d19251247058d65939ead74afbf5da7f11edce
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-04 14:14:47 -0300

    (feature): add ApiProperty to DTO class

--- src/app/todo/dto/create-todo.dto.ts
+++ src/app/todo/dto/create-todo.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
 
 export class CreateTodoDTO {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty({})
     task: string;
 
     @IsNotEmpty()
     @IsInt()
     @Min(0)
     @Max(1)
+    @ApiProperty()
     isDone: number;
 }
--- src/app/todo/dto/update-todo.dto.ts
+++ src/app/todo/dto/update-todo.dto.ts
@@ -1,12 +1,15 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
 
 export class UpdateTodoDTO {
     @IsNotEmpty()
     @IsString()
+    @ApiProperty()
     task: string;
 
     @IsInt()
     @Min(0)
     @Max(1)
+    @ApiPropertyOptional()
     isDone: number;
 }
