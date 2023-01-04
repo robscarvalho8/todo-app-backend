5dac94eed87b710e26228c158d3f0a0361580d8a
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-04 11:11:15 -0300

    (feature): create TodoEntity, and adjust for connection async with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,24 +4,24 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './app/todo/todo.module';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'postgres',
-        host: configService.get('POSTGRES_HOST'),
-        port: Number(configService.get('POSTGRES_PORT')),
-        database: configService.get('POSTGRES_DATABASE'),
-        username: configService.get('POSTGRES_USERNAME'),
-        password: configService.get('POSTGRES_PASSWORD'),
-        synchronize: true, // Not Recomended in Production
-        entities: [__dirname + '/**/*.entity{.js,.ts}'],
-      }),
-    }),
-    TodoModule,
-  ],
-  controllers: [],
-  providers: [],
+    imports: [
+        TypeOrmModule.forRootAsync({
+            imports: [ConfigModule.forRoot()],
+            useFactory: (configService: ConfigService) => ({
+                type: 'postgres',
+                host: configService.get('POSTGRES_HOST'),
+                port: +configService.get('POSTGRES_PORT'),
+                database: configService.get('POSTGRES_DATABASE'),
+                username: configService.get('POSTGRES_USERNAME'),
+                password: configService.get('POSTGRES_PASSWORD'),
+                entities: [__dirname + '/**/*.entity{.js,.ts}'],
+                synchronize: true, // Not Recomended in Production
+            }),
+            inject: [ConfigService],
+        }),
+        TodoModule,
+    ],
+    controllers: [],
+    providers: [],
 })
 export class AppModule {}
