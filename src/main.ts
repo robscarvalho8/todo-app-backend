cb6c142c141ee0b7432245401d4f9569533e4aea
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-04 14:14:14 -0300

    (feature): config swagger

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const config = new DocumentBuilder()
+        .setTitle('TODO API')
+        // .setDescription('The Cast API')
+        .setVersion('0.0.1')
+        // .addTag('api')
+        .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
     app.useGlobalPipes(new ValidationPipe());
     await app.listen(3000);
 }
