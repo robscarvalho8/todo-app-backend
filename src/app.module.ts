6f4b9aa29c9ba082400032a2b97803c6bb97d0b9
robscarvalho8 <robscarvalho8@gmail.com>
2023-01-04 10:45:48 -0300

    (feature): add connection with postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,24 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: Number(configService.get('POSTGRES_PORT')),
+        database: configService.get('POSTGRES_DATABASE'),
+        username: configService.get('POSTGRES_USERNAME'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        synchronize: true, // Not Recomended in Production
+        entities: [__dirname + '/**/*.entity{.js,.ts}'],
+      }),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
